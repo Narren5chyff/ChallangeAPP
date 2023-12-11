242abcf5f0b9b421ded99409c4360f9285e407ce
narrenschyff <kolofil@gmail.com>
2023-12-11 17:37:49 +0100

    Zadanie Dzień 8.

---
+++ ChallengeAPP/ChallengeAPP.tests/TypeTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeAPP.tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void LoginValueCheck()
+        {
+            var user1 = GetUser("Adam", "asdfgh");
+            var user2 = GetUser("Adam", "asdfgh");
+
+            Assert.AreEqual(user1.Login, user2.Login);
+        }
+        [Test]
+        public void DifferentUserCheck()
+        {
+            var user1 = GetUser("Adam", "asdfgh");
+            var user2 = GetUser("Adam", "asdfgh");
+
+            Assert.AreNotEqual(user1, user2);
+        }
+        [Test]
+        public void AreTwoNumbersEqual()
+        {
+            int number1 = 135;
+            int number2 = 135;
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        private User GetUser(string login, string password)
+        {
+            return new User(login, password);
+        }
+    }
+}
